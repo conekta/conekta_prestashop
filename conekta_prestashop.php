58f21c3ab586837e70c2324208dd735967357554
Eduardo Enriquez <eduardo@MacBook-Air-de-Eduardo-E.local>
2017-11-02 09:53:15 -0600

    Feature: Adding webhook notification file this change will include notification.php that support webhooks request our plugin endpoint to be able to change orders status
    Why is this change neccesary?
    
    this change is necesary because order statuses are not changing in a common order life cycle
    
    How does it address the issue?
    
    What side effects does this change have?
    this change maybe affect the upload to marketplace, the custom validation of marketplace
    rejet files like this

--- conekta_prestashop.php
+++ conekta_prestashop.php
@@ -713,7 +713,7 @@ class Conekta_Prestashop extends PaymentModule
 
 
         if (empty($url)) {
-            $url = _PS_BASE_URL_ . __PS_BASE_URI__ . "modules/conekta_prestashop/webhook/notification.php";
+            $url = _PS_BASE_URL_ . __PS_BASE_URI__ . "modules/conekta_prestashop/notification.php";
         }
 
         if (Tools::isSubmit('btnSubmit')) {
