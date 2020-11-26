27c31c5759b1690e8201642b2f7def3be2d4463e
mauricio <mauricio@serfe.com>
2020-11-26 15:03:42 -0300

    adding --user root

--- magento/commands/post-install.sh
+++ magento/commands/post-install.sh
@@ -3,8 +3,8 @@
 if [ -f ./env ]; then
   echo "There is no .env file into the root foler. Please review .env.sample file and setup the appropiate values"
 else
-  docker-compose exec phpfpm chown app:app -R . \
-  && docker-compose exec phpfpm chmod g+s -R . \
+  docker-compose exec --user root phpfpm chown app:app -R . \
+  && docker-compose exec --user root phpfpm chmod g+s -R . \
   && echo "Starting magento setup inside the container" \
   && docker-compose exec --user app phpfpm ./bin/setup.sh \
   && git checkout -- webroot/bin/magento
